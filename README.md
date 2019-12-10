1fe77eba17249d8690f96380ddb86c39e2cae58b
Francois Blondel <fblondel@vivaldicap.com>
2019-12-10 15:27:08 -0600

    Update readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2019
+
+Simple C# Solution with answers to the puzzle I was able to complete before getting crushed by the holidays!  
+Enjoy :)
--- Src/ConsoleUI/Program.cs
+++ Src/ConsoleUI/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using static System.Console;
 
 namespace AdventOfCode2019.ConsoleUI
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("*-*-*-*-*-*-*- Advent of Code 2019 -*-*-*-*-*-*-*");
-            Console.WriteLine("--- Day 1: The Tyranny of the Rocket Equation ---");
-            Console.WriteLine("                --- Part One ---                 ");
-            Console.WriteLine($"Sum of the fuel requirement (Modules): {PuzzleAnswers.Day1.Part1.GetResult()}");
-            Console.WriteLine("                --- Part Two ---                 ");
-            Console.WriteLine($"Sum of the fuel requirement (Modules + Fuel): {PuzzleAnswers.Day1.Part2.GetResult()}");
+            WriteLine("*-*-*-*-*-*-*- Advent of Code 2019 -*-*-*-*-*-*-*");
+            WriteLine("--- Day 1: The Tyranny of the Rocket Equation ---");
+            WriteLine("                --- Part One ---                 ");
+            WriteLine($"Sum of the fuel requirement (Modules): {PuzzleAnswers.Day1.Part1.GetResult()}");
+            WriteLine("                --- Part Two ---                 ");
+            WriteLine($"Sum of the fuel requirement (Modules + Fuel): {PuzzleAnswers.Day1.Part2.GetResult()}");
+            WriteLine("--- Day 2: 1202 Program Alarm ---");
+            WriteLine("                --- Part One ---                 ");
         }
     }
 }
